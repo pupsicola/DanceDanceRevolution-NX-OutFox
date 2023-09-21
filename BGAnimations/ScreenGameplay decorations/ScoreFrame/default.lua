830d4e111be9be169f40f8da10236e37c48700c0
pupsicola <56452648+pupsicola@users.noreply.github.com>
2023-09-20 21:42:53 -0500

    Update default.lua

--- BGAnimations/ScreenGameplay decorations/ScoreFrame/default.lua
+++ BGAnimations/ScreenGameplay decorations/ScoreFrame/default.lua
@@ -5,7 +5,7 @@ local t = Def.ActorFrame{
 --Players
 for _,pn in pairs(GAMESTATE:GetEnabledPlayers()) do
     t[#t+1] = Def.ActorFrame{
-        InitCommand=cmd(zoom,0.75;x,SCREEN_CENTER_X-377;y,SCREEN_CENTER_Y+332;addy,100;sleep,0;decelerate,2;addy,-100;);
+        InitCommand=cmd(zoom,0.75;x,Center1Player() and SCREEN_CENTER_X-8 or SCREEN_CENTER_X-377;y,SCREEN_CENTER_Y+332;addy,100;sleep,0;decelerate,2;addy,-100;);
 		Def.ActorFrame{
 			LoadActor("score_counter",pn);
 		};
